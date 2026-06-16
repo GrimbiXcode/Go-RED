2a37f01c04430ea30d329d687837ce2387d703a6
GrimbiXcode <david@grimbichler.ch>
2026-06-16 10:17:24 +0200

    feat(web): Add App, index, and Tailwind CSS files

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FlowEditor } from './components/FlowEditor';
+import { FlowProvider } from './components/FlowProvider';
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-gray-50">
+      <FlowProvider>
+        <FlowEditor />
+      </FlowProvider>
+    </div>
+  );
+}
+
+export default App;
