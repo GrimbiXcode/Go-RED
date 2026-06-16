cbfe542d4986bb6c531c4c318ff5e469ee551d55
GrimbiXcode <david@grimbichler.ch>
2026-06-17 00:57:31 +0200

    Fix: Remove unused React import in FlowEditor.tsx

--- web/src/components/FlowEditor.tsx
+++ web/src/components/FlowEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useFlowContext } from './FlowProvider';
 import { FlowCanvas } from './FlowCanvas';
 import { NodePalette } from './NodePalette';
